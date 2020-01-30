41cd45b2ba07fb2927a6bb48566b12e66f296da6
Conor Fox <cf26@csg20-21.ucc.ie>
2020-01-30 16:09:00 +0000

    Template

---
+++ Other/template.py
@@ -0,0 +1,22 @@
+#!/usr/local/bin/python3
+
+from cgitb import enable
+enable()
+
+# Other imports can go here
+
+print("Content-type: text/html")
+print()
+
+# The heart of the program goes here, e.g. get the user data, do something with it
+
+print("""
+    <!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="utf-8" />
+            <title>Your title goes here</title>
+        </head>
+        <body>
+        </body>
+    </html>""")
