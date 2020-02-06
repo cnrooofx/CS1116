313aadc97543ad77b315f99bcb02336fb2336c09
Conor <conor@conorfox.com>
2020-02-06 20:20:51 +0000

    Lab 2

--- lab2/lengths.py
+++ lab2/lengths.py
@@ -1,10 +1,9 @@
 #!/usr/local/bin/python3
 
-from cgitb import enable
-enable()
-
 from cgi import FieldStorage
 from html import escape
+from cgitb import enable
+enable()
 
 print('Content-Type: text/html')
 print()
@@ -14,40 +13,45 @@ length = escape(form_data.getfirst('length', '')).strip()
 units = escape(form_data.getfirst('units', '')).strip()
 
 output = ''
-try:
-    if units == 'feet':
-        feet = float(length)
-        inches = feet * 12
-        yards = feet / 3
-    elif units == 'inches':
-        inches = float(length)
-        feet = inches / 12
-        yards = inches / 36
-    else:
-        yards = float(length)
-        inches = yards * 36
-        feet = yards * 3
-    output = """ <table>
-                <tr>
-                    <th colspan="2" scope="col">Results</th>
-                </tr>
-                <tr>
-                    <th scope="row">Inches:</th>
-                    <td>%.2f</td>
-                </tr>
-                <tr>
-                    <th scope="row">Feet:</th>
-                    <td>%.2f</td>
-                </tr>
-                <tr>
-                    <th scope="row">Yards:</th>
-                    <td>%.2f</td>
-                </tr>
-            </table>""" % (inches, feet, yards)
-except ValueError:
-    output = """<p>
-                Please enter a number
-            </p>"""
+inches = 0.0
+yards = 0.0
+feet = 0.0
+
+if units in ['feet', 'inches', 'yards']:
+    try:
+        if units == 'feet':
+            feet = float(length)
+            inches = feet * 12
+            yards = feet / 3
+        elif units == 'inches':
+            inches = float(length)
+            feet = inches / 12
+            yards = inches / 36
+        else:
+            yards = float(length)
+            inches = yards * 36
+            feet = yards * 3
+        output = """ <table>
+                    <tr>
+                        <th colspan="2" scope="col">Results</th>
+                    </tr>
+                    <tr>
+                        <th scope="row">Inches:</th>
+                        <td>%.2f</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Feet:</th>
+                        <td>%.2f</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Yards:</th>
+                        <td>%.2f</td>
+                    </tr>
+                </table>""" % (inches, feet, yards)
+    except ValueError:
+        output = '<p>Error! Please enter a number</p>'
+else:
+    output = '<p>Error! Invalid units</p>'
 
 
 print("""
