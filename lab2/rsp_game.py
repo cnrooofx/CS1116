df40f6fe033025206e6eab804cb42ae0ac8ddecd
Conor <conor@conorfox.com>
2020-02-09 09:45:43 +0000

    Remove two player

--- lab2/rsp_game.py
+++ lab2/rsp_game.py
@@ -9,9 +9,7 @@ print('Content-Type: text/html')
 print()
 
 form_data = FieldStorage()
-players = form_data.getfirst('players', '')
-choice1 = form_data.getfirst('rsp1', '')
-choice2 = form_data.getfirst('rsp2', '')
+choice = form_data.getfirst('rsp', '')
 
 rsp_dict = {0: 'Rock', 1: 'Fire', 2: 'Scissors', 3: 'Snake', 4: 'Human',
             5: 'Tree', 6: 'Wolf', 7: 'Sponge', 8: 'Paper', 9: 'Air',
@@ -36,122 +34,33 @@ extras_dict = {6: {12: "\'s heiney"}, 7: {9: " pockets"}}
 
 outcome = ''
 extras = ''
-form = ''
 
 if len(form_data) != 0:
-    form = '''
-            <form action='rsp_game.py' type='get'>
-            <fieldset>
-                <legend>Player 1</legend>
-                <input type="radio" name="rsp1" value="0" id="rock" checked />
-                <label for="rock">Rock</label>
-                <input type="radio" name="rsp1" value="1" id="fire" />
-                <label for="fire">Fire</label>
-                <input type="radio" name="rsp1" value="2" id="scissors" />
-                <label for="scissors">Scissors</label>
-                <input type="radio" name="rsp1" value="3" id="snake" />
-                <label for="snake">Snake</label>
-                <input type="radio" name="rsp1" value="4" id="human" />
-                <label for="human">Human</label>
-                <input type="radio" name="rsp1" value="5" id="tree" />
-                <label for="tree">Tree</label>
-                <input type="radio" name="rsp1" value="6" id="wolf" />
-                <label for="wolf">Wolf</label>
-                <input type="radio" name="rsp1" value="7" id="sponge" />
-                <label for="sponge">Sponge</label>
-                <input type="radio" name="rsp1" value="8" id="paper" />
-                <label for="paper">Paper</label>
-                <input type="radio" name="rsp1" value="9" id="air" />
-                <label for="air">Air</label>
-                <input type="radio" name="rsp1" value="10" id="water" />
-                <label for="water">Water</label>
-                <input type="radio" name="rsp1" value="11" id="dragon" />
-                <label for="dragon">Dragon</label>
-                <input type="radio" name="rsp1" value="12" id="devil" />
-                <label for="devil">Devil</label>
-                <input type="radio" name="rsp1" value="13" id="lightning" />
-                <label for="lightning">Lightning</label>
-                <input type="radio" name="rsp1" value="14" id="gun" />
-                <label for="gun">Gun</label>
-            </fieldset>'''
-    if players == '1':
-        form += '''
-            <fieldset>
-                <legend>Player 2</legend>
-                <input type="radio" name="rsp2" value="0" id="rock" checked />
-                <label for="rock">Rock</label>
-                <input type="radio" name="rsp2" value="1" id="fire" />
-                <label for="fire">Fire</label>
-                <input type="radio" name="rsp2" value="2" id="scissors" />
-                <label for="scissors">Scissors</label>
-                <input type="radio" name="rsp2" value="3" id="snake" />
-                <label for="snake">Snake</label>
-                <input type="radio" name="rsp2" value="4" id="human" />
-                <label for="human">Human</label>
-                <input type="radio" name="rsp2" value="5" id="tree" />
-                <label for="tree">Tree</label>
-                <input type="radio" name="rsp2" value="6" id="wolf" />
-                <label for="wolf">Wolf</label>
-                <input type="radio" name="rsp2" value="7" id="sponge" />
-                <label for="sponge">Sponge</label>
-                <input type="radio" name="rsp2" value="8" id="paper" />
-                <label for="paper">Paper</label>
-                <input type="radio" name="rsp2" value="9" id="air" />
-                <label for="air">Air</label>
-                <input type="radio" name="rsp2" value="10" id="water" />
-                <label for="water">Water</label>
-                <input type="radio" name="rsp2" value="11" id="dragon" />
-                <label for="dragon">Dragon</label>
-                <input type="radio" name="rsp2" value="12" id="devil" />
-                <label for="devil">Devil</label>
-                <input type="radio" name="rsp2" value="13" id="lightning" />
-                <label for="lightning">Lightning</label>
-                <input type="radio" name="rsp2" value="14" id="gun" />
-                <label for="gun">Gun</label>
-            </fieldset>'''
-    form += '''<input type="submit"  /></form>'''
-    if len(choice1) != 0 or len(choice2) != 0:
-        try:
-            name1 = 'Player 1\'s'
-            player1 = int(choice1)
-            if players == '0':
-                player2 = randint(0, 14)
-                name2 = 'Computer\'s'
-            elif players == '1':
-                player2 = int(choice2)
-                name2 = 'Player 2\'s'
-            if player1 in rsp_dict:
-                player1_out = rsp_dict[player1]
-                player2_out = rsp_dict[player2]
-                if player1 != player2:
-                    i = 0
-                    while i <= 7:
-                        if player2 == ((player1 + i) % 15):
-                            if (player1 == 6 and player2 == 12) or (player1 == 7 and player2 == 9):
-                                extras = extras_dict[player2][player2]
-                            outcome = '%s %s %s %s %s%s' % (name1, player1_out, beats_dict[player1][player2], name2, player2_out, extras)
-                            break
-                        i += 1
-                    else:
-                        if (player2 == 6 and player1 == 12) or (player2 == 7 and player1 == 9):
-                            extras = extras_dict[player2][player1]
-                        outcome = '%s %s %s %s %s%s' % (name2, player2_out, beats_dict[player2][player1], name1,  player1_out, extras)
+    try:
+        user = int(choice)
+        if user in rsp_dict:
+            user_out = rsp_dict[user]
+            computer = randint(0, 14)
+            comp_out = rsp_dict[computer]
+            if computer != user:
+                i = 0
+                while i <= 7:
+                    if computer == ((user + i) % 15):
+                        if (user == 6 and computer == 12) or (user == 7 and computer == 9):
+                            extras = extras_dict[user][computer]
+                        outcome = 'User\'s %s %s Computer\'s %s%s' % (user_out, beats_dict[user][computer], comp_out, extras)
+                        break
+                    i += 1
                 else:
-                    outcome = '%s %s ties with %s %s' % (name2, player2_out, name1, player1_out)
+                    if (computer == 6 and user == 12) or (computer == 7 and user == 9):
+                        extras = extras_dict[computer][user]
+                    outcome = 'Computer\'s %s %s User\'s %s%s' % (comp_out, beats_dict[computer][user], user_out, extras)
             else:
-                outcome = 'Error! Incorrect number'
-        except ValueError:
-            outcome = 'Error! Please enter a number'
-else:
-    form = '''
-            <form action="rsp_game.py" method="get">
-                <input type="radio" name="players" value="0" id="computer">
-                <label for="computer">Player vs Computer</label>
-                <input type="radio" name="players" value="1" id="player">
-                <label for="player">Player vs Player</label>
-                <input type="submit" />
-            </form>'''
-
+                outcome = 'Computer\'s %s ties with User\'s %s' % (comp_out, user_out)
+        else:
+            outcome = 'Error! Incorrect number'
+    except ValueError:
+        outcome = 'Error! Please enter a number'
 
 print("""
     <!DOCTYPE html>
@@ -162,9 +71,42 @@ print("""
             <title>Lengths</title>
         </head>
         <body>
-            %s
+            <form action="rsp_game.py" method="get">
+                <p>Rock Fire Scissors Snake etc</p>
+                <input type="radio" name="rsp" value="0" id="rock" checked />
+                <label for="rock">Rock</label>
+                <input type="radio" name="rsp" value="1" id="fire" />
+                <label for="fire">Fire</label>
+                <input type="radio" name="rsp" value="2" id="scissors" />
+                <label for="scissors">Scissors</label>
+                <input type="radio" name="rsp" value="3" id="snake" />
+                <label for="snake">Snake</label>
+                <input type="radio" name="rsp" value="4" id="human" />
+                <label for="human">Human</label>
+                <input type="radio" name="rsp" value="5" id="tree" />
+                <label for="tree">Tree</label>
+                <input type="radio" name="rsp" value="6" id="wolf" />
+                <label for="wolf">Wolf</label>
+                <input type="radio" name="rsp" value="7" id="sponge" />
+                <label for="sponge">Sponge</label>
+                <input type="radio" name="rsp" value="8" id="paper" />
+                <label for="paper">Paper</label>
+                <input type="radio" name="rsp" value="9" id="air" />
+                <label for="air">Air</label>
+                <input type="radio" name="rsp" value="10" id="water" />
+                <label for="water">Water</label>
+                <input type="radio" name="rsp" value="11" id="dragon" />
+                <label for="dragon">Dragon</label>
+                <input type="radio" name="rsp" value="12" id="devil" />
+                <label for="devil">Devil</label>
+                <input type="radio" name="rsp" value="13" id="lightning" />
+                <label for="lightning">Lightning</label>
+                <input type="radio" name="rsp" value="14" id="gun" />
+                <label for="gun">Gun</label>
+                <input type="submit"  />
+            </form>
             <p>
                 %s
             </p>
         </body>
-    </html>""" % (form, outcome))
+    </html>""" % (outcome))
