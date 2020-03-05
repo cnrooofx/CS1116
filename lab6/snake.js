efd155c6a595a0acac8df888c936a664fcf1c289
Conor <conor@conorfox.com>
2020-03-05 21:03:04 +0000

    Initial Lab

---
+++ lab6/snake.js
@@ -0,0 +1,133 @@
+let canvas;
+let context;
+let width;
+let height;
+let intervalId;
+
+let snakes = [];
+
+let size;
+let length = 1;
+let prev_x;
+let prev_y;
+let moveUp = false;
+let moveDown = false;
+let moveLeft = false;
+let moveRight = false;
+
+document.addEventListener('DOMContentLoaded', init, false);
+
+function init() {
+    canvas = document.querySelector('canvas');
+    context = canvas.getContext('2d');
+    width = canvas.width;
+    height = canvas.height;
+    size = (width / grid.length);
+    // intervalId = window.setInterval(draw, 66);
+    window.setInterval(draw, 66)
+    window.addEventListener('keydown', activate, false);
+}
+
+let grid = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+
+let snake = {
+    x : getRandomNumber(2, 22),
+    y : getRandomNumber(2, 22),
+    colour : 'green',
+    age : 1,
+}
+
+let apple = {
+    colour : 'red',
+    x : getRandomNumber(2, 22),
+    y : getRandomNumber(2, 22),
+}
+
+function draw() {
+    context.clearRect(0, 0, width, height);
+    prev_x = snake.x;
+    prev_y = snake.y;
+    grid[snake.y][snake.x] = 1;
+    // grid[prev_y][prev_x] = 0;
+    let row_number = 0;
+    for (let row of grid) {
+        let cell_number = 0;
+        for (let cell of row) {
+            if (cell === 1) {
+                context.fillRect((cell_number*size)+2, (row_number*size)+2, size-2, size-2)
+            }
+            cell_number++;
+        }
+        row_number++
+    }
+    if (moveUp) {
+        snake.y--;
+    } else if (moveDown) {
+        snake.y++;
+    } else if (moveLeft) {
+        snake.x--;
+    } else if (moveRight) {
+        snake.x++;
+    }
+    if (grid[snake.y][snake.x] === 1) {
+        stop()
+    }
+    length++;
+}
+
+function activate(event) {
+    let KeyCode = event.keyCode;
+    if (KeyCode === 37) {
+        moveLeft = true;
+        moveUp = false;
+        moveDown = false;
+        moveRight = false;
+    } else if (KeyCode === 38) {
+        moveUp = true;
+        moveDown = false;
+        moveLeft = false;
+        moveRight = false;
+    } else if (KeyCode === 39) {
+        moveRight = true;
+        moveUp = false;
+        moveDown = false;
+        moveLeft = false;
+    } else if (KeyCode === 40) {
+        moveDown = true;
+        moveUp = false;
+        moveLeft = false;
+        moveRight = false;
+    }
+}
+
+function stop() {
+    window.clearInterval(intervalId);
+}
+
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
