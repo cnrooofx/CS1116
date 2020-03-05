a39c4657ce5c23829ad9872a6daf9d3d853809b2
Conor <conor@conorfox.com>
2020-03-05 07:44:56 +0000

    Working!

--- lab5/exB.js
+++ lab5/exB.js
@@ -4,9 +4,10 @@ let width;
 let height;
 
 let balls = [];
-let radius = 30;
+let radius;
+let e = 0.8; // Coefficient of restitution
 let gravity = 0.2;
-let time = 20;
+let time = 33;
 
 document.addEventListener('DOMContentLoaded', init, false);
 
@@ -15,12 +16,9 @@ function init() {
     context = canvas.getContext('2d');
     width = canvas.width = (window.innerWidth - 30);
     height = canvas.height = (window.innerHeight - 30);
-    radius = (width / 50)
+    radius = (width / 40);
     window.setInterval(draw, time);
 }
-
-// Velocity = Displacement / Time
-//          =
 
 function draw() {
     if (balls.length < 10) {
@@ -28,30 +26,31 @@ function draw() {
             x : getRandomNumber(radius+15, (width-radius+15)),
             y : getRandomNumber(radius+15, (height-radius+15)),
             r : (radius + getRandomNumber(0, 15)),
-            xChange : getRandomNumber(-10, 10),
-            yChange : getRandomNumber(-10, 10),
+            velocityX : getRandomNumber(-10, 10),
+            velocityY : getRandomNumber(-5, 5),
             colour : 'rgb('+getRandomNumber(0, 255)+','+getRandomNumber(0, 255)+','+getRandomNumber(0, 255)+')',
             mass : 0,
-            // velocity : 0
+            collided : false,
         }
-        ball.mass = (Math.PI * (ball.r ** 2));
-        //      velocity = displacement / time  Pixels per milisecond?
-        // ball.velocity = ((Math.sqrt((ball.xChange ** 2) + (ball.yChange ** 2))) / (time));
+        ball.mass = ball.r ** 2;
+        if (velocityX === 0) {
+            velocityX++
+        }
+        while (occupied(ball)) {
+            ball.x = getRandomNumber(radius+15, (width-radius+15));
+            ball.y = getRandomNumber(radius+15, (width-radius+15));
+        }
         balls.push(ball);
     }
     context.clearRect(0, 0, width, height);
     for (let ball of balls) {
-        ball.x += ball.xChange;
-        ball.y += ball.yChange;
-        ball.yChange += gravity;
+        ball.x += ball.velocityX;
+        ball.y += ball.velocityY;
+        ball.velocityY += gravity;
         drawBall(ball);
         for (let ball2 of balls) {
             if (near(ball, ball2)) {
-                collides(ball, ball2)
-                // let v_ball = (((ball.velocity * (ball.mass - ball2.mass)) + (2 * ball2.mass * ball2.velocity)) / (ball.mass + ball2.mass));
-                // let v_ball2 = (((ball2.velocity * (ball2.mass - ball.mass)) + (2 * ball.mass * ball.velocity)) / (ball.mass + ball2.mass));
-                // console.log(v_ball);
-                // console.log(v_ball2);
+                collides(ball, ball2);
             }
         }
         edges(ball);
@@ -63,46 +62,62 @@ function near(ball, ball2) {
     if ((ball.x + radii > ball2.x) &&
     (ball.x < radii + ball2.x) &&
     (ball.y + radii > ball2.y) &&
-    (ball.y < radii + ball2.y)) {
+    (ball.y < radii + ball2.y) &&
+    ((! ball.collided) && (! ball2.collided))) {
         return true;
+    } else {
+        ball.collided = false;
     }
 }
 
 function collides(ball, ball2) {
-    let ball_next_x = (ball.x + ball.xChange);
-    let ball_next_y = (ball.y + ball.yChange);
-    let ball2_next_x = (ball2.x + ball2.xChange);
-    let ball2_next_y = (ball2.y + ball2.yChange);
+    let ball_next_x = (ball.x + ball.velocityX);
+    let ball_next_y = (ball.y + ball.velocityY);
+    let ball2_next_x = (ball2.x + ball2.velocityX);
+    let ball2_next_y = (ball2.y + ball2.velocityY);
     let distance = Math.sqrt((((ball_next_x - ball2_next_x) ** 2) + ((ball_next_y - ball2_next_y) ** 2)));
     if ((distance <= (ball.r + ball2.r)) &&
-    ((ball2.xChange !== 0) && (ball2.yChange !== 0)))  {
-        ball.xChange *= -1;
-        ball.yChange = ((ball.yChange * -1) + gravity);
-        ball2.xChange *= -1;
-        ball2.yChange = ((ball2.yChange * -1) + gravity);
+    ((ball2.velocityX !== 0) && (ball2.velocityY !== 0)))  {
+        // let collisionX = ((ball.x * ball2.r) + (ball2.x * ball.r)) / (ball.r + ball2.r);
+        // let collisionY = ((ball.y * ball2.r) + (ball2.y * ball.r)) / (ball.r + ball2.r);
+        let newVelocityX = ((ball.velocityX * e) * (ball.mass - ball2.mass) +
+          (2 * ball2.mass * ball2.velocityX)) / (ball.mass + ball2.mass);
+        let newVelocityY = ((ball.velocityY * e) * (ball.mass - ball2.mass) +
+          (2 * ball2.mass * ball2.velocityY)) / (ball.mass + ball2.mass);
+        let newVelocityX2 = ((ball2.velocityX * e) * (ball2.mass - ball.mass) +
+          (2 * ball.mass * ball.velocityX)) / (ball.mass + ball2.mass);
+        let newVelocityY2 = ((ball2.velocityY * e) * (ball2.mass - ball.mass) +
+          (2 * ball.mass * ball.velocityY)) / (ball.mass + ball2.mass);
+        ball.velocityX = newVelocityX;
+        ball.velocityY = newVelocityY;
+        ball2.velocityX = newVelocityX2;
+        ball2.velocityY = newVelocityY2;
+        ball2.colour = ball.colour
+        ball.collided = true;
+        ball2.collided = true;
     }
 }
 
 function edges(ball) {
     // Top & Bottom
-    if (((((ball.y) - ball.r) < 0) && ball.yChange < 0) ||
-    ((((ball.y) + ball.r) > height) && ball.yChange > 0)) {
-        ball.yChange *= -1;
-        ball.yChange += gravity;
+    if (((((ball.y) - ball.r) < 0) && ball.velocityY < 0) ||
+    ((((ball.y) + ball.r) > height) && ball.velocityY > 0)) {
+        ball.velocityY *= -1;
+        changeColour(ball);
     // Right & Left
-  } else if (((((ball.x) + ball.r) > width) && ball.xChange > 0) ||
-    ((((ball.x) - ball.r) < 0) && ball.xChange < 0)) {
-        ball.xChange = (ball.xChange * -1);
-        ball.yChange += gravity;
-        ball.xChange += gravity;
-        return true;
+    } else if (((((ball.x) + ball.r) > width) && ball.velocityX > 0) ||
+    ((((ball.x) - ball.r) < 0) && ball.velocityX < 0)) {
+        ball.velocityX *= -1;
+        changeColour(ball);
     }
 }
 
 function drawBall(ball) {
-    context.fillStyle = ball.colour;
+    context.strokeStyle = '#000';
+    context.lineWidth = 0.5;
     context.beginPath();
     context.arc(ball.x, ball.y, ball.r, 0, (2 * Math.PI));
+    context.fillStyle = ball.colour;
     context.fill();
     context.shadowBlur = 10;
     context.shadowColor = 'rgba(153,153,153, 0.8)';
@@ -110,10 +125,28 @@ function drawBall(ball) {
     context.shadowOffsetY = 2;
 }
 
+function occupied(ball) {
+    // Top Left x - r  y - r
+    // Top Right x + r  y - r
+    // Bottom Left x - r  y + r
+    // Bottom Right x + r  y + r
+    for (let otherball of balls) {
+        let boxSize = (ball.r + otherball.r);
+        if ((ball.x < otherball.x + boxSize) &&
+        (ball.x > otherball.x - boxSize) &&
+        (ball.y < otherball.y + boxSize) &&
+        (ball.y > otherball.y - boxSize)) {
+            // New ball x too close to another ball
+            return true;
+        }
+    }
+    return false;
+}
+
+function changeColour(ball) {
+    ball.colour = 'rgb('+getRandomNumber(0, 255)+','+getRandomNumber(0, 255)+','+getRandomNumber(0, 255)+')';
+}
+
 function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
-// V2 – V1 = -e (U 2– U1)
-
-// M1 U1 + M2 U2 = M1 V1 + M 2V2
