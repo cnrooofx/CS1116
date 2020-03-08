2974ddc72c6a00ccb9f5eeb2952ca075355ceede
Conor Fox <conor@conorfox.com>
2020-03-08 17:12:31 +0000

    Clean up - remove extra comments

--- lab5/exB.js
+++ lab5/exB.js
@@ -69,11 +69,6 @@ function draw() {
     }
 }
 
-// if ((ball.x < otherball.x + boxSize) &&
-// (ball.x > otherball.x - boxSize) &&
-// (ball.y < otherball.y + boxSize) &&
-// (ball.y > otherball.y - boxSize)) {
-
 function near(ball, ball2) {
     if ((ball.x - ball.r < ball2.x + ball2.r) &&
     (ball.x + ball.r > ball2.x - ball2.r) &&
@@ -91,14 +86,8 @@ function near(ball, ball2) {
 }
 
 function collides(ball, ball2) {
-    // let ball_next_x = (ball.x + ball.velocityX);
-    // let ball_next_y = (ball.y + ball.velocityY);
-    // let ball2_next_x = (ball2.x + ball2.velocityX);
-    // let ball2_next_y = (ball2.y + ball2.velocityY);
     let distance = Math.sqrt((((ball.x - ball2.x) ** 2) + ((ball.y - ball2.y) ** 2)));
     if (distance <= (ball.r + ball2.r))  {
-        // let collisionX = ((ball.x * ball2.r) + (ball2.x * ball.r)) / (ball.r + ball2.r);
-        // let collisionY = ((ball.y * ball2.r) + (ball2.y * ball.r)) / (ball.r + ball2.r);
         let newVelocityX = ((ball.velocityX * e) * (ball.mass - ball2.mass) +
           (2 * ball2.mass * ball2.velocityX)) / (ball.mass + ball2.mass);
         let newVelocityY = ((ball.velocityY * e) * (ball.mass - ball2.mass) +
@@ -107,16 +96,10 @@ function collides(ball, ball2) {
           (2 * ball.mass * ball.velocityX)) / (ball.mass + ball2.mass);
         let newVelocityY2 = ((ball2.velocityY * e) * (ball2.mass - ball.mass) +
           (2 * ball.mass * ball.velocityY)) / (ball.mass + ball2.mass);
-        // context.beginPath();
-        // context.moveTo(ball.x, ball.y);
-        // context.lineTo(ball2.x, ball2.y);
-        // context.stroke();
         ball.velocityX = newVelocityX;
         ball.velocityY = newVelocityY;
         ball2.velocityX = newVelocityX2;
         ball2.velocityY = newVelocityY2;
-        // changeColour(ball);
-        // changeColour(ball2);
         ball.colour = ball2.colour
         ball.collided = true;
         ball2.collided = true;
@@ -157,10 +140,6 @@ function drawBall(ball) {
 }
 
 function occupied(ball) {
-    // Top Left x - r  y - r
-    // Top Right x + r  y - r
-    // Bottom Left x - r  y + r
-    // Bottom Right x + r  y + r
     for (let otherball of balls) {
         let boxSize = (ball.r + otherball.r);
         if ((ball.x < otherball.x + boxSize) &&
