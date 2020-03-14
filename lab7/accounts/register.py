018078fb790b7305fa553325c60903bc63a71bef
Conor <conor@conorfox.com>
2020-03-14 18:14:32 +0000

    Registration working

--- lab7/accounts/register.py
+++ lab7/accounts/register.py
@@ -2,77 +2,71 @@
 
 from cgi import FieldStorage, escape
 from hashlib import sha256
-from time import time
-from shelve import open
-from http.cookies import SimpleCookie
 import pymysql as db
+from passwordgen import generate_password
+from send_email import password_email
+
 from cgitb import enable
 enable()
 
+print('Content-Type: text/html')
+print()
+
 form_data = FieldStorage()
+name = ''
 username = ''
+email = ''
 result = ''
+
 if len(form_data) != 0:
+    name = escape(form_data.getfirst('name', '').strip())
     username = escape(form_data.getfirst('username', '').strip())
-    password1 = escape(form_data.getfirst('password1', '').strip())
-    password2 = escape(form_data.getfirst('password2', '').strip())
-    if not username or not password1 or not password2:
-        result = '<p>Error: user name and passwords are required</p>'
-    elif password1 != password2:
-        result = '<p>Error: passwords must be equal</p>'
+    email = escape(form_data.getfirst('email', '').strip())
+    if not name or not username or not email:
+        result = '<p>Sorry! All fields are required.</p>'
     else:
         try:
-            connection = db.connect('localhost', 'userid', 'password', 'database_name')
+            connection = db.connect('localhost', 'cf26', 'pecah', 'cs6503_cs1106_cf26')
             cursor = connection.cursor(db.cursors.DictCursor)
             cursor.execute("""SELECT * FROM users
                               WHERE username = %s""", (username))
             if cursor.rowcount > 0:
-                result = '<p>Error: user name already taken</p>'
+                result = """<p>Sorry! That username is already taken.</p>
+                            <p>Choose another username or <a href='login.py'>Click here</a> to sign in.</p>"""
             else:
-                sha256_password = sha256(password1.encode()).hexdigest()
-                cursor.execute("""INSERT INTO users (username, password)
-                                  VALUES (%s, %s)""", (username, sha256_password))
+                password = generate_password()
+                sha256_password = sha256(password.encode()).hexdigest()
+                cursor.execute("""INSERT INTO users (username, name, email, password)
+                                  VALUES (%s, %s, %s, %s)""", (username, name, email, sha256_password))
                 connection.commit()
                 cursor.close()
                 connection.close()
-                cookie = SimpleCookie()
-                sid = sha256(repr(time()).encode()).hexdigest()
-                cookie['sid'] = sid
-                session_store = open('sess_' + sid, writeback=True)
-                session_store['authenticated'] = True
-                session_store['username'] = username
-                session_store.close()
+                password_email(name, email, password)
                 result = """
-                   <p>Succesfully inserted!</p>
-                   <p>Thanks for joining Web Dev 2.</p>
-                   <ul>
-                       <li><a href="protected_page_A.py">Web Dev 2 - Members Only A</a></li>
-                       <li><a href="protected_page_B.py">Web Dev 2 - Members Only B</a></li>
-                       <li><a href="logout.py">Logout</a></li>
-                   </ul>"""
-                print(cookie)
+                   <p>You have been successfully registered!</p>
+                   <p>Please check your email to get your password</p>"""
         except (db.Error, IOError):
-            result = '<p>Sorry! We are experiencing problems at the moment. Please call back later.</p>'
+            result = '<p>Sorry! We are experiencing problems at the moment. Please try again later.</p>'
 
-print('Content-Type: text/html')
-print()
 print("""
     <!DOCTYPE html>
-    <html lang="en">
+    <html lang='en'>
         <head>
-            <meta charset="utf-8" />
+            <meta charset='utf-8' />
             <title>Web Dev 2</title>
         </head>
         <body>
-            <form action="register.py" method="post">
-                <label for="username">User name: </label>
-                <input type="text" name="username" id="username" value="%s" />
-                <label for="password1">Password: </label>
-                <input type="password" name="password1" id="password1" />
-                <label for="passwords2">Re-enter password: </label>
-                <input type="password" name="password2" id="password2" />
-                <input type="submit" value="Register" />
+            <form action='register.py' method='post'>
+                <fieldset>
+                    <label for='name'>Name: </label>
+                    <input type='text' name='name' id='name' value='%s' required />
+                    <label for='username'>Username: </label>
+                    <input type='text' name='username' id='username' value='%s' required />
+                    <label for='email'>Email: </label>
+                    <input type='email' name='email' id='email' value='%s' required />
+                    <input type='submit' value='Register' />
+                </fieldset>
             </form>
             %s
         </body>
-    </html>""" % (username, result))
+    </html>""" % (name, username, email, result))
