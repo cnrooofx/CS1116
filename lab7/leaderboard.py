88a3a17fc8fd23a2bcfbcd49b800c2cfaa074fd8
Conor <conor@conorfox.com>
2020-03-14 13:07:56 +0000

    Framework files for site

---
+++ lab7/leaderboard.py
@@ -0,0 +1,35 @@
+#!/usr/local/bin/python3
+
+import pymysql as db
+from cgitb import enable
+enable()
+
+print('Content-Type: text/html')
+print()
+
+result = ''
+try:
+    connection = db.connect('localhost', 'cf26', 'pecah', 'cs6503_cs1106_cf26')
+    cursor = connection.cursor(db.cursors.DictCursor)
+    cursor.execute("""SELECT candidate_name, total_votes
+                      FROM candidates
+                      ORDER BY total_votes DESC, candidate_name ASC""")
+    result = '<table><tr><th>Candidate name</th><th>Total votes</th></tr>'
+    for row in cursor.fetchall():
+        result += '<tr><td>%s</td><td>%i</td></tr>' % (row['candidate_name'], row['total_votes'])
+    result += '</table>'
+    cursor.close()
+    connection.close()
+except db.Error:
+    result = '<p>Sorry! We are experiencing problems at the moment. Please call back later.</p>'
+
+print("""
+    <!DOCTYPE html>
+        <html lang="en">
+            <head>
+                <title>Leaderboard</title>
+            </head>
+            <body>
+                %s
+            </body>
+            </html>""" % (result))
