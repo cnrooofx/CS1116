055903f3e8f3ab190162843f7cbaaad9a472e3e9
Conor <conor@conorfox.com>
2020-04-10 13:52:17 +0100

    Sprite textures

--- lab7/game.js
+++ lab7/game.js
@@ -18,44 +18,46 @@ let player = {
     move_down :  false,
     move_left : false,
     move_right : false,
+    sprite : 0,
 }
 let monster = {
     x : 2,
     y : 2,
     damage : 25,
-    health : 100,
     move_up : false,
     move_down :  false,
     move_left : false,
     move_right : false,
 }
 
+let sprites;
+
 let levels = [[[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
   [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1],
+  [1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 6, 0],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1],
+  [1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1],
+  [1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
   [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1],
-  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
-  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1],
-  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
-  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1],
-  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
-  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1],
-  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]],
 
  [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -94,19 +96,20 @@ function init() {
     main = document.querySelector('main');
     width = 0.9 * Math.min(main.clientWidth, main.clientHeight);
     canvas.height = canvas.width = width;
+    sprites = new Image();
+    sprites.src = 'media/sprites.png'
     grid_size = 27;
     size = width / grid_size;
     newGrid();
     level = 0;
-    change_level(level);
 
     player.x = 13;
     player.y = 13;
     player.health = 100;
+    player.sprite = 0;
     monster.x = 2;
     monster.y = 2;
     monster.damage = 25;
-    monster.health = 100;
     monster.move_speed = 5;
 
     player.move_up = false;
@@ -114,10 +117,11 @@ function init() {
     player.move_left = false;
     player.move_right = false;
     monster.move_up = false;
-    monster.move_down = false;
+    monster.move_down = true;
     monster.move_left = false;
-    monster.move_right = true;
+    monster.move_right = false;
 
+    change_level(level);
     interval_id = window.setInterval(draw, 100);
     window.addEventListener('keydown', activate, false);
     window.addEventListener('keyup', deactivate, false);
@@ -148,14 +152,15 @@ function build() {
         for (let j = 0; j < grid_size; j += 1) {
             let value = grid[j][i];
             if (value === 5) {
-                context.fillStyle = 'orange';
-                context.fillRect(size*i, size*j, size, size)
+                context.drawImage(sprites, player.sprite, 48, 16, 16, size*i, size*j, size, size)
             } else if (value === 4) {
-                context.fillStyle = 'red';
-                context.fillRect(size*i, size*j, size, size)
+                context.drawImage(sprites, 0, 64, 16, 16, size*i, size*j, size, size)
+            } else if (value === 6) {
+                context.drawImage(sprites, 0, 16, 16, 16, size*i, size*j, size, size)
             } else if (value === 1) {
-                context.fillStyle = 'white';
-                context.fillRect(size*i, size*j, size, size)
+                context.drawImage(sprites, 0, 0, 16, 16, size*i, size*j, size, size)
+            } else if (value === 0) {
+                context.drawImage(sprites, 16, 0, 16, 16, size*i, size*j, size, size)
             }
         }
     }
@@ -165,11 +170,17 @@ function move(character) {
         if (grid[character.y-1][character.x] !== 1) {
             grid[character.y][character.x] = 0;
             character.y -= 1;
+        } else if (character === monster) {
+            character.move_up = false;
+            character.move_down = true;
         }
     } else if (character.move_down) {
         if (grid[character.y+1][character.x] !== 1) {
             grid[character.y][character.x] = 0;
             character.y += 1;
+        } else if (character === monster) {
+            character.move_down = false;
+            character.move_up = true;
         }
     } else if (character.move_left) {
         if (grid[character.y][character.x-1] !== 1) {
@@ -194,15 +205,19 @@ function move(character) {
         grid[character.y][character.x] = 4
     }
 }
-// function monster(direction) {
-//     if (direction === 'vertical') {
-//         return
-//     } else if (direction === 'horizontal') {
-//         if (grid[monster.y][monster.x+1] !== 1) {
-//             monster.x += 1
-//         }
-//     }
-// }
+function damage() {
+    for (let x = player.x; x < player.x+3; x += 1) {
+        for (let y = player.y; x < player.y+3; y += 1) {
+            if (grid[y][x] === 4) {
+                if (player.health-monster.damage < 0) {
+                    stop()
+                } else {
+                    player.health -= monster.damage;
+                }
+            }
+        }
+    }
+}
 function newGrid() {
     grid = []
     for (let i = 0; i < grid_size; i += 1) {
@@ -221,8 +236,8 @@ function change_level(num) {
     }
     player.x = 13;
     player.y = 13;
-    grid[player.x][player.y] = 5;
-    grid[monster.x][monster.y] = 4;
+    grid[player.y][player.x] = 5;
+    grid[monster.y][monster.x] = 4;
 }
 function stop() {
     window.alert('Game Over :(');
@@ -235,10 +250,12 @@ function activate(event) {
     let KeyCode = event.keyCode;
     if (KeyCode === 37) {
         player.move_left = true;
+        player.sprite = 0;
     } else if (KeyCode === 38) {
         player.move_up = true;
     } else if (KeyCode === 39) {
         player.move_right = true;
+        player.sprite = 16;
     } else if (KeyCode === 40) {
         player.move_down = true;
     }
