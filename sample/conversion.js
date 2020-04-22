72993d16f0aff4ae310ff8a1e893aaf75c34f965
Conor <conor@conorfox.com>
2020-04-22 13:05:16 +0100

    Derek's Corrections for Q2

--- sample/conversion.js
+++ sample/conversion.js
@@ -1,64 +1,50 @@
-let feet_input;
-let feet_span;
-let inches_input;
-let inches_span;
-let metres_box;
+let feet;
+let inches;
+let metres;
+let message1;
+let message2;
 let form_element;
-let feet_msg;
-let inches_msg;
-let feet_int;
-let inches_int;
-let metres_int;
 
 document.addEventListener('DOMContentLoaded', init, false);
 
 function init() {
-    feet_input = document.getElementById('feet');
-    feet_span = document.getElementById('message1');
-    inches_input = document.getElementById('inches');
-    inches_span = document.getElementById('message2');
-    metres_box = document.getElementById('metres');
+    feet = document.getElementById('feet');
+    inches = document.getElementById('inches');
+    metres = document.getElementById('metres');
+    message1 = document.getElementById('message1');
+    message2 = document.getElementById('message2');
     form_element = document.querySelector('form');
-    form_element.addEventListener('submit', convert_to_metres, false);
-    feet_input.addEventListener('change', validate_input, false);
-    inches_input.addEventListener('change', validate_input, false);
+    form_element.addEventListener('submit', convert, false);
 }
 
-function validate_input(event) {
-    feet_msg = check_for_int(feet_input.value, 0, 8);
-    inches_msg = check_for_int(inches_input.value, 0, 11);
-    feet_span.innerHTML = feet_msg;
-    inches_span.innerHTML = inches_msg;
-}
-
-function convert_to_metres(event) {
-    feet_msg = check_for_int(feet_input.value, 0, 8);
-    inches_msg = check_for_int(inches_input.value, 0, 11);
-    feet_span.innerHTML = feet_msg;
-    inches_span.innerHTML = inches_msg;
-    if (! feet_msg && ! inches_msg) {
-        feet_int = Number(feet_input.value);
-        inches_int = Number(inches_input.value);
-        metres_int = ((feet_int * 12) + inches_int) * 0.0254;
-        metres_box.value = metres_int;
+function convert(event) {
+    let feet_error = validate(feet, 0, 10);
+    let inches_error = validate(inches, 0, 11);
+    message1.innerHTML = feet_error;
+    message2.innerHTML = inches_error;
+    if (feet_error === '' && inches_error === '') {
+        let feet_int = Number(feet.value);
+        let inches_int = Number(inches.value);
+        let metres_int = ((feet_int * 12) + inches_int) * 0.0254;
+        metres.value = metres_int;
     }
     event.preventDefault();
 }
 
-function check_for_int(text, minimum, maximum) {
-    let trimmed_text = text.trim();
-    if (trimmed_text === "") {
-        return "Required";
+function validate(element, min, max) {
+    let value = element.value.trim();
+    let error = '';
+    if (value === '') {
+        error = 'Required'
+    } else {
+        let number = ~~Number(value);
+        if (String(number) !== value) {
+            error = 'Must be a whole number';
+        } else if (number < min) {
+            error = 'Must be greater than ' + min;
+        } else if (number > max) {
+            error = 'Must be less than ' + max;
+        }
     }
-    let number = ~~Number(trimmed_text);
-    if (String(number) !== trimmed_text) {
-        return "Must be a whole number";
-    }
-    if (number < minimum) {
-        return "Must be no less than " + minimum;
-    }
-    if (number > maximum) {
-        return "Must be no greater than " + maximum;
-    }
-    return '';
+    return error
 }
